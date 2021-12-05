7d2c4e4b118fe333e89bb751fd848f8de4365ffc
Rainer Wiesenfarth <Rainer_Wiesenfarth@trimble.com>
2021-12-05 11:04:40 +0100

    initial commit

---
+++ Day03/main.cpp
@@ -0,0 +1,142 @@
+
+#include <fstream>
+#include <iostream>
+#include <vector>
+
+
+void splitList( const std::vector<int> &input, int bitNumber, std::vector<int> &ones, std::vector<int> &zeroes )
+{
+  int mask = 1 << bitNumber;
+
+  for( auto value : input )
+  {
+    if( value & mask )
+    {
+      ones.push_back( value );
+    }
+    else
+    {
+      zeroes.push_back( value );
+    }
+  }
+}
+
+
+int oxygenGeneratorRating( const std::vector<int> &input, int bitCount )
+{
+  std::vector<int> currentInput = input;
+  int              currentBit   = bitCount;
+
+  do
+  {
+    std::vector<int> ones;
+    std::vector<int> zeroes;
+
+    currentBit--;
+    splitList( currentInput, currentBit, ones, zeroes );
+    if( ones.size() >= zeroes.size() )
+    {
+      currentInput = ones;
+    }
+    else
+    {
+      currentInput = zeroes;
+    }
+  }
+  while( currentInput.size() > 1 );
+
+  std::cout << "Oxygen generator rating = " << currentInput[0] << std::endl;
+
+  return currentInput[0];
+}
+
+
+int co2ScrubberRating( const std::vector<int> &input, int bitCount )
+{
+  std::vector<int> currentInput = input;
+  int              currentBit = bitCount;
+
+  do
+  {
+    std::vector<int> ones;
+    std::vector<int> zeroes;
+
+    currentBit--;
+    splitList( currentInput, currentBit, ones, zeroes );
+    if( zeroes.size() <= ones.size() )
+    {
+      currentInput = zeroes;
+    }
+    else
+    {
+      currentInput = ones;
+    }
+  }
+  while( currentInput.size() > 1 );
+
+  std::cout << "CO2 scrubber rating = " << currentInput[0] << std::endl;
+
+  return currentInput[0];
+}
+
+
+int main()
+{
+  std::ifstream    input( R"(D:\Develop\AdventOfCode2021\Day03\input.txt)" );
+  std::string      line;
+  std::vector<int> bitsSet;
+  std::vector<int> values;
+  int              lineCount = 0;
+
+  while( !input.eof() )
+  {
+    input >> line;
+    lineCount++;
+    //std::cout << "read: '" << line << "'" << std::endl;
+
+    if( bitsSet.empty() )
+    {
+      bitsSet.resize( line.size(), 0 );
+    }
+
+    int value = 0;
+    for( int i = 0; i < line.size(); i++ )
+    {
+      value <<= 1;
+      if( line[i] == '1' )
+      {
+        value++;
+        bitsSet[i]++;
+      }
+    }
+    values.push_back( value );
+  }
+
+  int gamma   = 0;
+  int epsilon = 0;
+
+  for( auto ones : bitsSet )
+  {
+    gamma   <<= 1;
+    epsilon <<= 1;
+
+    if( ones > lineCount / 2 )
+    {
+      gamma++;
+    }
+    else
+    {
+      epsilon++;
+    }
+  }
+
+  std::cout << "result: gamma = " << gamma << ", epsilon = " << epsilon << ", power consumption = " <<
+    gamma * epsilon << std::endl;
+
+  auto oxy = oxygenGeneratorRating( values, bitsSet.size() );
+  auto co2 = co2ScrubberRating( values, bitsSet.size() );
+
+  std::cout << "life support rating = " << oxy * co2 << std::endl;
+
+  return 0;
+}
---
+++ Day05/main.cpp
@@ -0,0 +1,145 @@
+
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <vector>
+#include <string>
+
+
+static constexpr int s_boardSize = 5;
+
+
+struct BingoEntry
+{
+  bool m_drawn = false;
+  int  m_value = 0;
+
+  explicit BingoEntry( int value )
+    : m_value( value )
+  { }
+};
+
+
+struct BingoBoard
+{
+  BingoBoard( std::ifstream &input )
+  {
+    m_entries.resize( s_boardSize );
+    m_rowHits.resize( s_boardSize, 0 );
+    m_colHits.resize( s_boardSize, 0 );
+    for( auto &row : m_entries )
+    {
+      for( int col = 0; col < s_boardSize; col++ )
+      {
+        int value;
+        input >> value;
+        row.emplace_back( BingoEntry( value ) );
+      }
+    }
+  }
+
+  bool markAndCheckWin( int value )
+  {
+    for( int r = 0; r < s_boardSize; r ++ )
+    {
+      auto &row = m_entries[r];
+
+      for( int c = 0; c < s_boardSize; c++ )
+      {
+        auto &col = row[c];
+
+        if( col.m_value == value )
+        {
+          col.m_drawn = true;
+          m_rowHits[r]++;
+          m_colHits[c]++;
+        }
+      }
+    }
+    for( int i = 0; i < s_boardSize; i++ )
+    {
+      if( ( m_rowHits[i] == s_boardSize ) || ( m_colHits[i] == s_boardSize ) )
+      {
+        m_hasWon = true;
+        break;
+      }
+    }
+    return m_hasWon;
+  }
+
+  int sumUnmarked()
+  {
+    int sum = 0;
+
+    for( const auto &row : m_entries )
+    {
+      for( const auto &col : row )
+      {
+        if( !col.m_drawn )
+        {
+          sum += col.m_value;
+        }
+      }
+    }
+    return sum;
+  }
+
+  std::vector<std::vector<BingoEntry>> m_entries;
+  std::vector<int>                     m_rowHits;
+  std::vector<int>                     m_colHits;
+  bool                                 m_hasWon = false;
+};
+
+
+int main()
+{
+  std::ifstream           input( R"(D:\Develop\AdventOfCode2021\Day04\input.txt)" );
+  std::string             drawnLine;
+  std::vector<int>        drawnValues;
+  std::vector<BingoBoard> boards;
+
+  std::getline( input, drawnLine );
+  while( true )
+  {
+    drawnValues.push_back( std::stoi( drawnLine ) );
+
+    auto pos = drawnLine.find( ',' );
+    if( pos == std::string::npos )
+    {
+      break;
+    }
+    else
+    {
+      drawnLine = drawnLine.substr( pos + 1 );
+    }
+  }
+
+  while( !input.eof() )
+  {
+    boards.emplace_back( BingoBoard( input ) );
+  }
+
+  std::cout << "Read " << drawnValues.size() << " values and " << boards.size() << " boards." << std::endl;
+
+  int  finalScore = 0;
+  bool isFirst    = true;
+  for( auto value : drawnValues )
+  {
+    for( auto &board : boards )
+    {
+      if( !board.m_hasWon && board.markAndCheckWin( value ) )
+      {
+        finalScore = value * board.sumUnmarked();
+        if( isFirst )
+        {
+          std::cout << "Final score of first board won is " << finalScore << std::endl;
+          isFirst = false;
+        }
+      }
+    }
+  }
+
+  std::cout << "Final score of last board won is " << finalScore << std::endl;
+
+  return 0;
+}
